56372e6a9403d64854c10302dd92b277241f1a0c
David Ozmanian <dmozmanyan@gmail.com>
2021-08-21 20:34:58 +0300

    initial commit

---
+++ LoopCollectionView/LoopCollectionView.h
@@ -0,0 +1,18 @@
+//
+//  LoopCollectionView.h
+//  LoopCollectionView
+//
+//  Created by David Ozmanian on 21.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoopCollectionView.
+FOUNDATION_EXPORT double LoopCollectionViewVersionNumber;
+
+//! Project version string for LoopCollectionView.
+FOUNDATION_EXPORT const unsigned char LoopCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoopCollectionView/PublicHeader.h>
+
+
